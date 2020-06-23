8bb7c8346b38fe02192f2fd3f3c2b49a5af34376
Ivan Teo <46380612+ivaanteo@users.noreply.github.com>
2020-06-23 15:19:39 +0800

    added api key input file

---
+++ Stock Portfolio/Model/stockDataApiKeyInput.swift
@@ -0,0 +1,12 @@
+//
+//  stockDataApiKey.swift
+//  Stock Portfolio
+//
+//  Created by Ivan Teo on 23/6/20.
+//  Copyright © 2020 Ivan Teo. All rights reserved.
+//
+
+import Foundation
+extension StockDataManager{
+    // static let apikey = "your api key here"
+}
